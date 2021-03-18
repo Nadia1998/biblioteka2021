4495f3512c3333b5441407f352fe83f6caf78a11
Nadia1998 <nadjacoguric998@gmail.com>
2021-03-18 17:13:32 +0100

    Ispravljanje gresaka.

---
+++ test/biblioteka/interfejs/BibliotekaInterfaceTest.java
@@ -0,0 +1,14 @@
+package biblioteka.interfejs;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BibliotekaInterfaceTest {
+
+	@Test
+	final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
